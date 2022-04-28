91db79379016260925b2ca65c44719b2efb9bfd9
Pete Seeling <peteseeling@gmail.com>
2022-04-28 12:23:28 -0600

    setup /listen/close server written. Test resp chng

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,14 +4,19 @@
 import { screen, render } from '@testing-library/react'
 // 🚨
 // import rest
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { rest } from 'msw'
 // import setupServer
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { setupServer } from 'msw/node'
+
 import App from './App'
 
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: '',
+  name: 'Test User',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -20,12 +25,17 @@ const user = {
 }
 
 // 🚨 Create your server
+const server = setupServer(
+  rest.get(
+    `${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users`,
+    (req, res, ctx) => res(ctx.json([user]))
+  ))
 
 // 🚨 Listen for server start
-beforeAll()
+beforeAll(() => server.listen())
 
 // 🚨 Close server when complete
-afterAll()
+afterAll(() => server.resetHandlers())
 
 test('Should render the header', async () => {
   render(<App />)
@@ -53,6 +63,12 @@ test('Should render the header with Sasuke 🌬️🔥', async () => {
   }
 
   // 🚨 Use the server to change the response for this test
+  server.use(
+    rest.get(
+      `${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users`,
+      (req, res, ctx) => res(ctx.json([sasuke]))
+    )
+  )
 
   render(<App />)
 
